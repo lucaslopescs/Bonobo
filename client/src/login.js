e483f4c7860f1f4cc0f67432ae845b0a7fc67a3f
KyleMains <164123670+KyleMains@users.noreply.github.com>
2024-11-04 13:37:23 -0500

    Added login js file

---
+++ client/src/login.js
@@ -0,0 +1,58 @@
+//verify does not work
+
+//variables to test verify
+let userInput = "";
+let passInput = "";
+let correctUser = "test";
+let correctPass = "password";
+
+function SignInButton() {
+  return (
+    <button onClick={verify}>Login</button>
+  );
+}
+
+function UsernameBox(){
+  return([
+  <label for="uname">Username: </label>,
+  userInput = <input type="text" id="uname" name="uname" placeholder="Enter username"></input>]
+  );
+}
+function PasswordBox(){
+  return([
+  <label for="pword">Password: </label>,
+  passInput = <input type="text" id="pword" name="pword" placeholder="Enter password"></input>]
+  );
+}
+
+function verify(){
+  console.log(userInput);
+  console.log(passInput);
+ /**  if(userInput == correctUser){
+    if(passInput == correctPass){
+      return console.log("logging in");
+    }
+    else{
+      return console.log("wrong password");
+    }
+  }
+  else{ 
+    return console.log("wrong user");
+  } */
+} 
+
+
+function App() {
+  return (
+    <div>
+        <h1>Welcome! Please enter your account information</h1>
+        <UsernameBox />
+        <div></div>
+        <PasswordBox />
+        <div></div>
+        <SignInButton />
+    </div>
+  );
+}
+
+export default App;
