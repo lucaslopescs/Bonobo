b23c39f343007d20ccfb2a5045bcb7670b599318
KyleMains <kmains02@gmail.com>
2024-11-21 15:10:41 -0500

    added nonfuncitonal search bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import Popup from 'reactjs-popup';
 import Calendar from './calendar';
 import axios from 'axios';
-import Login from './login';
+import Login from './Login';
 
 function App() {
   const [message, setMessage] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [search, setSearch] = useState('');
   const [userRole, setUserRole] = useState(null);
   const [username, setUsername] = useState('');
 
@@ -20,11 +22,6 @@ function App() {
         console.error('Error fetching data:', error);
       });
   }, []);
-
- // const handleLoginSuccess = (role, username) => {
-   // console.log(`User ${username} logged in as ${role}`);
-  //  setCurrentUser({ username, role });
-  //};
 
 
   const handleLoginSuccess = (response) => {
@@ -41,6 +38,10 @@ function App() {
     alert('You have logged out successfully.');
   };
 
+const handleSearch = () => {
+  alert('You have searched for: ' + search);
+}
+
   return (
     <div className="App">
       {!isLoggedIn ? (
@@ -49,14 +50,13 @@ function App() {
         <div className="dashboard">
            <h2>Welcome, {username}!
              <p>Role: {userRole}
-               <button 
-                 onClick={handleLogout} 
-                 className="logout-button"
-                 >
-                Logout
-                </button>
+                <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value) } className="search-bar"/>  
+                <button onClick={handleSearch} className="search-button">Search</button>
+                
+                <button onClick={handleLogout} className="logout-button">Logout</button>
              </p>
            </h2>
+
            {userRole === 'faculty' && (
               <div className="faculty-controls">
                 <button onClick={() => {/* Add event handler */}}>Create Event</button>
@@ -67,8 +67,10 @@ function App() {
           <Calendar userRole={userRole} /> 
         </div>
       )}
+      
     </div>
+    
   );
 }
-
+//                <button onClick={handleSearch} className="search-button">Search</button>
 export default App;
